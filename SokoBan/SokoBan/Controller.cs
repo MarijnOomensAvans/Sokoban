ae959d2288087d636c5d5a162edaabbb9a5780b4
Gijs Verwiel <gijsverwiel@hotmail.com>
2018-09-10 16:59:38 +0200

    Start

---
+++ SokoBan/SokoBan/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SokoBan
+{
+    public class Controller
+    {
+        private InputView inputView;
+        private OutputView outputView;
+        private Maze maze;
+
+        public Controller()
+        {
+            inputView = new InputView();
+            outputView = new OutputView();
+
+        }
+    }
+}
---
+++ SokoBan/SokoBan/Parser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SokoBan
+{
+    public class Parser
+    {
+        private Maze maze;
+
+        public Parser()
+        {
+            maze = new Maze();
+        }
+    }
+}
