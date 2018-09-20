c86663fa3c3fdcdf14e472c935749c9001edbea2
Gijs Verwiel <gijsverwiel@hotmail.com>
2018-09-20 15:25:08 +0200

    Added InputView#GetDirection()

--- SokoBan/SokoBan/View/InputView.cs
+++ SokoBan/SokoBan/View/InputView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace SokoBan
@@ -32,6 +33,45 @@ namespace SokoBan
                 mazeNumber = -1;
             }
             return mazeNumber;
+        }
+
+        public int GetDirection()   //returns 1 for arrow up, 2 for arrow down, 3 for left, 4 for right
+        {                           //returns -1 for S, and -2 for R
+            bool pressed = false;
+            int result = 0;
+            while (!pressed)
+            {
+                ConsoleKey input = Console.ReadKey(false).Key;
+
+                switch (input)
+                {
+                    case ConsoleKey.S:
+                        result = - 1;
+                        pressed = true;
+                        break;
+                    case ConsoleKey.R:
+                        result = - 2;
+                        pressed = true;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        result = 1;
+                        pressed = true;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        result = 2;
+                        pressed = true;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        result = 3;
+                        pressed = true;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        result = 4;
+                        pressed = true;
+                        break;
+                }
+            }
+            return result;
         }
     }
 }
