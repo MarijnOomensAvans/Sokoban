625bb6b096387cf24f9663da885253923393a509
Gijs Verwiel <gijsverwiel@hotmail.com>
2018-09-24 10:26:16 +0200

    !

--- SokoBan/SokoBan/Model/Maze.cs
+++ SokoBan/SokoBan/Model/Maze.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -11,5 +12,56 @@ namespace SokoBan
         public List<Crate> CrateList { get; set; }
         public List<Tile> TileList { get; set; }
         public Tile OriginPoint { get; set; }
+
+        public Maze(int level)
+        {
+            string txtFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString();
+            String[] MazeStringArray =
+                System.IO.File.ReadAllLines(@"" + txtFile + "/Sokoban/Doolhoffen/doolhof" + level + ".txt");
+
+            bool FirstLine = true;
+            bool FirstChar = true;
+
+            foreach (string MazeLine in MazeStringArray)
+            {
+                FirstChar = true;
+                foreach (char c in MazeLine)
+                {
+                    Tile t;
+                    switch (c)
+                    { 
+                        case '.':
+                            t = new FloorTile();
+                            break;
+                        case 'o':
+                            t = new FloorTile();
+                            t.Crate = new Crate();
+                            break;
+                        case '#':
+                            t = new Wall();
+                            break;
+                        case 'x': 
+                            t = new EndTile();
+                            break;
+                        case '@': 
+                            t = new FloorTile();
+                            Player = new Player(t);
+                            t.Player = Player;
+                            break;
+                        default: 
+                            t = new EmptyTile();
+                            break;
+                    }
+
+                    if (FirstLine)
+                    {
+                        if (FirstChar)
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
