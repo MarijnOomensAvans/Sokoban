d1f92fa4073b49b20da1e47e63dff44b072f69c3
MarijnOomensAvans <mdoomens@avans.nl>
2018-09-24 11:43:34 +0200

    Weg

--- SokoBan/SokoBan/Model/Maze.cs
+++ SokoBan/SokoBan/Model/Maze.cs
@@ -13,57 +13,6 @@ namespace SokoBan
         public List<Tile> TileList { get; set; }
         public Tile OriginPoint { get; set; }
 
-        public Maze(int level)
-        {
-            string txtFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString();
-            String[] MazeStringArray =
-                System.IO.File.ReadAllLines(@"" + txtFile + "/Sokoban/Doolhoffen/doolhof" + level + ".txt");
-
-            bool FirstLine = true;
-            bool FirstChar = true;
-
-            foreach (string MazeLine in MazeStringArray)
-            {
-                FirstChar = true;
-                foreach (char c in MazeLine)
-                {
-                    Tile t;
-                    switch (c)
-                    { 
-                        case '.':
-                            t = new FloorTile();
-                            break;
-                        case 'o':
-                            t = new FloorTile();
-                            t.Crate = new Crate();
-                            break;
-                        case '#':
-                            t = new Wall();
-                            break;
-                        case 'x': 
-                            t = new EndTile();
-                            break;
-                        case '@': 
-                            t = new FloorTile();
-                            Player = new Player(t);
-                            t.Player = Player;
-                            break;
-                        default: 
-                            t = new EmptyTile();
-                            break;
-                    }
-
-                    if (FirstLine)
-                    {
-                        if (FirstChar)
-                        {
-
-                        }
-                    }
-                }
-            }
-        }
-
         public void movePlayer(int direction)
         {
             Player.MoveTo(direction);
