a868baffee1248a22fa8439c2c6433456ad97f47
MarijnOomensAvans <mdoomens@avans.nl>
2018-10-05 11:26:53 +0200

    Optimizations (alle imports weg)

--- SokoBan/SokoBan/App/Program.cs
+++ SokoBan/SokoBan/App/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace SokoBan
+﻿namespace SokoBan
 {
     class Program
     {
--- SokoBan/SokoBan/Model/Tile.cs
+++ SokoBan/SokoBan/Model/Tile.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SokoBan
+﻿namespace SokoBan
 {
     public abstract class Tile
     {
